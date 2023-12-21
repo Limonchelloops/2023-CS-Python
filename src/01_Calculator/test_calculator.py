6beca00946c0182114d966823f696b2618d146c7
Limonchelloops <fox21vek@yandex.ru>
2023-12-21 16:33:45 +1000

    Update test_calculator.py

--- src/01_Calculator/test_calculator.py
+++ src/01_Calculator/test_calculator.py
@@ -20,9 +20,9 @@ def test_prefix_evaluate(equation: str, result: int):
 @pytest.mark.parametrize(
     ("infix_equation", "prefix_equation"),
     [
-        ("1 + 2", "+ 1 2"),
-        ("1 * 2", "* 1 2"),
-        ("1 + ( 2 - 3 ) * 2", "+ 1 * - 2 3 2"),
+        ("1 + 2", ['+', '1', '2']),
+        ("1 * 2", ['*', '1', '2']),
+        ("1 + ( 2 - 3 ) * 2", ['+', '1', '*', '-', '2', '3', '2']),
     ],
 )
 def test_to_prefix(infix_equation: str, prefix_equation: str):
@@ -31,4 +31,4 @@ def test_to_prefix(infix_equation: str, prefix_equation: str):
 
 @pytest.mark.parametrize("equation", ["1 + 2", "1 * 2", "1 + ( 2 - 3 ) * 2"])
 def test_calculate(equation: str):
-    assert calculate(equation) == eval(equation)  # noqa: S307
+    assert calculate(equation) == eval(equation)
